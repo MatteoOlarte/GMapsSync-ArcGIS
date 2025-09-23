a0717449a502dbe04bd9f6673cb90f1c686828a9
MatteoOlarte <mateoc09+2@gmail.com>
2025-09-23 15:18:11 -0500

    feat: add configuration button with its settings images

---
+++ SIGUE Google-Sync/Presentation/View/ConfigButton.cs
@@ -0,0 +1,17 @@
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace GMapsSync.Presentation.View;
+
+internal class ConfigButton : Button
+{
+    protected override void OnClick()
+    {
+        var data = new object[] { "Configuración" };
+
+        if (!PropertySheet.IsVisible)
+        {
+            PropertySheet.ShowDialog("GMapsSync_ConfigSheet", "GMapsSync_ConfigPage", data);
+        }
+    }
+}
